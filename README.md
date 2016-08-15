25e2709b7ea267cb6bb17dc8c7b1b7131f4b6cae
Danielle H <daheber@gmail.com>
2016-08-15 16:43:14 -0700

    cowsay in the html

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Basic Webpack Angular App
+
+This is my first application using webpack and angular.
+
+## Dirctions to run locally
+
+1. `npm install`to install components
+2. `npm start` to start the server
+3. open your browser to `localhost:8080`
--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
 import './main.css';
 import angular from 'angular';
-import getCow from './cow';
+//import getCow from './cow';
+import cowsay from 'cowsay-browser';
 
 const app = angular.module('myApp', []);
 
 app.controller('main', function($scope){
   $scope.name = 'world';
+  $scope.cow = function(){
+    return cowsay.think({
+      text: 'Cows are cool',
+      e: 'oO',
+      T: 'U'
+    });
+  };
+
 });
 
-console.log(getCow());
+//console.log(getCow());
