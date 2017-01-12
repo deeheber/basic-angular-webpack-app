50ab073af63cdcc8e5aabe8b93168d6ab26fd389
Danielle H <daheber@gmail.com>
2017-01-11 19:52:30 -0800

    playing with filters

--- src/app.js
+++ src/app.js
@@ -18,6 +18,22 @@ app.controller('main', function($scope){
       T: 'U'
     });
   };
+  $scope.dogs = [{name: 'Bert', color: 'yellow', gender: 'M'}, {name: 'Elmo', color: 'red', gender: 'F'}, {name: 'Ernie', color: 'orange', gender: 'M'}];
+  $scope.genderFilter = function(value){
+    console.log(value);
+    return function(dog){
+      // could probably do this with a switch
+      if(value.Female && value.Male){
+        return dog;
+      } else if(value.Female){
+        return dog.gender == 'F';
+      } else if(value.Male){
+        return dog.gender == 'M';
+      } else {
+        return $scope.dogs;
+      }
+    };
+  };
 });
 
 console.log(getTest());
